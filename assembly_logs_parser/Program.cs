f7bcdccf960c97548eec1b287eb59dd406482a81
Sergey <enclave2005@gmail.com>
2023-04-05 22:51:54 +0500

    загружчик бд перенёс с дпугое приложение

--- assembly_logs_parser/Program.cs
+++ assembly_logs_parser/Program.cs
@@ -12,77 +12,8 @@ namespace assembly_logs_parser
     internal class Program
     {
 
-        #region Структура БД ассамблеи
-        /* 
-        [STAT_VSP_CONF] - статистика конференции
-            SEANCE_NO=int
-            CLUSTER_ID=int
-            SCHEME_ID=int
-            START_TYPE_ID=int
-            DT_BEGIN=timestamp (ex. - 1671868800)
-            DT_END=timestamp (ex. - 1671877734)
-            DT_COLLECTION=timestamp (ex. - 1671868822)
-            STARTUSER_ID#
-            STARTUSER_NAME#
-            IDENTIFY_CODE=int
-            FULL_DURATION=int
-            WORK_DURATION=int
-            CLUSTER_NAME=Новый кластер
-            SCHEME_NAME=string (ex. - Название селектора)
-            CONTRACT_NO=
-            RESERVED_CH=int
-            RESERVED_DSP=int
-            RESERVED_TIME=int
-            IS_PLANCONF=int
-            FINISH_REASON=int
-            FINISH_USER_ID#
-            FINISH_USER_NAME# 
-        */
-
-        /*
-        [STAT_VSP_CONF_PLAN] - планировщик
-            CLUSTER_ID=int
-            ID=int
-            MODIFY_NO=int
-            DT_ACTION=timestamp (ex. - 1671953100)
-            DT_BEGIN=timestamp (ex. - 1671953100)
-            DT_END=timestamp (ex. - 1671954600)
-            DSP_RES_ID=int
-            CH_CNT=int
-            DSPRES_CNT=int
-            SCHEME_ID=int
-            AUTO_START=int
-            SERIA_ID=int
-            IS_MODIFIED_IN_SERIA=int
-            CLUSTER_NAME=Новый кластер
-            SCHEME_NAME=string (ex. - Название селектора)
-            ACTION_TYPE=int
-            TASK_ID=int
-            USER_LOGIN#
-            STAT_SEANCE_NO = int
-            STAT_RESULT=int
-            ACTUAL_CH_CNT=int
-            ACTUAL_DSPRES_CNT=int
-        */
-        /*
-        [VSP_CONF_SCHEMES]
-            CLUSTER_ID=int
-            ID=int
-            NAME=string (ex. - Название селектора)
-            IDENTIFY_CODE =int
-            NP_MAX_CH=int
-            NP_MAX_DSP=int
-            NP_MAX_TIME=int
-        */
-        #endregion
-
         static Dictionary<string, List<string>> settings_dictionary = new Dictionary<string, List<string>> { };
 
-
-        static Dictionary<int, conference> conferences = new Dictionary<int, conference> { };
-
-
-        static string data_base_filename = "";
         static string processed_log_files = Path.GetFullPath("assemblylogsparser_temp_folder") + "\\processed_log_files.txt";
 
         static void Main(string[] args)
@@ -97,54 +28,6 @@ namespace assembly_logs_parser
             
             Console.ReadKey();
         }
-
-        private static void load_data_base()
-        {
-            Dictionary<string, List<List<string>>> data_base_dictionary = new Dictionary<string, List<List<string>>> { };
-
-            string[] data_rtdb = File.ReadAllLines(Path.GetFullPath(settings_dictionary["data_base"].First()));
-            string last_key = "";
-            List<string> data = new List<string>(); // значения для одной записи
-            foreach (string db_string in data_rtdb)
-            {
-                if (db_string != "") // записи в БД разделены пустой строкой
-                {
-                    if (db_string[0] == '[') // название таблицы в квадратных скобках
-                    {
-                        last_key = db_string.Split(']')[0].Replace('[', ' ').Trim(); // запоминаем название таблицы и делаем его ключом в словаре
-                        if (!data_base_dictionary.ContainsKey(last_key)) // если такого ключа нет, добавлем ключ и пустой список значений
-                        {
-                            data_base_dictionary.Add(last_key, new List<List<string>> { });
-                        }
-                    }
-                    else
-                    {
-                        data.Add(db_string.Trim());
-                        //      data_base_dictionary[last_key].Add(db_string.Trim());
-                    }
-                }
-                else
-                {
-                    data_base_dictionary[last_key].Add(data);
-                    data = new List<string>();
-                }
-            }
-
-            foreach (List<string> item in data_base_dictionary["VSP_CONF_SCHEMES"])
-            {
-                conference conf = new conference(item);
-                if (!conferences.ContainsKey(conf.ID))
-                {
-                    conferences.Add(conf.ID, conf);
-                }
-                else
-                {
-                    add_to_main_log("в словаре конференций одинаковые ID: [" + conf.ID.ToString() + " " + conf.Name + "] и [" + conferences[conf.ID].ID.ToString() + " " + conferences[conf.ID].Name);
-                }
-            }
-        }
-
-
 
         private static void add_to_main_log(string text_log, bool add_to_text_log = true)
         {
@@ -173,7 +56,7 @@ namespace assembly_logs_parser
                 File.AppendAllText(processed_log_files, "# logs файлы ассамблеи, которые были обработаны программой" + "\r\n");
             }
 
-            List<string> fields_list = new List<string> { };
+           // List<string> fields_list = new List<string> { };
             if (File.Exists("assembly_logs_parser_settings.ini"))
             {
                 string[] settings_file_lines = File.ReadAllLines("assembly_logs_parser_settings.ini");
@@ -206,7 +89,6 @@ namespace assembly_logs_parser
                     no_errors = false;
                 }
 
-                data_base_filename = Path.GetFileName(settings_dictionary["data_base"].First());
 
             }
             else
