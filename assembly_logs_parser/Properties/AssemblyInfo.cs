fd6c2944b0250859756b72d35e8501233735237b
Fargelex <enclave3005@hotmail.com>
2023-04-11 10:03:53 +0500

    О программе

--- assembly_logs_parser/Properties/AssemblyInfo.cs
+++ assembly_logs_parser/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("assembly_logs_parser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Автоматическая обработка лог-файлов ПК Ассамблея")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("assembly_logs_parser")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCompany("ПАО Сургутнефтегаз")]
+[assembly: AssemblyProduct("Удостоверение на рацпредложение №320 от 20.02.2021")]
+[assembly: AssemblyCopyright("© 2021 С.А.Саяпин")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("ru-RU")]
